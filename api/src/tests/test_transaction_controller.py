a017de9937bc0974f5ec1f2820d6db989cf187a6
Victor Ivan <viictor.ivan@gmail.com>
2021-10-06 19:23:18 -0300

    chore: adding the test when file is invalid on post/transaction

--- api/src/tests/test_transaction_controller.py
+++ api/src/tests/test_transaction_controller.py
@@ -31,3 +31,17 @@ def test_when_the_type_of_file_is_not_txt(client):
     )
     assert resp.status_code == 400
     assert resp.json == {'msg': 'Your file needs to be a .txt'}
+
+def test_when_file_is_not_an_CNAB_file(client):
+
+    """ If the request was did with a file that was not a CNAB file,
+        but is a .txt, needs to return an error
+    """
+
+    invalidCNAB = open(path.join(here, 'files/invalid_CNAB.txt'), 'rb')
+    resp = client.post('/transaction',
+        headers={'Content-Type': 'multipart/form-data'},
+        data={'file': invalidCNAB }
+    )
+    assert resp.status_code == 400
+    assert resp.json == {'msg': 'Your file is not a valid CNAB file'}
