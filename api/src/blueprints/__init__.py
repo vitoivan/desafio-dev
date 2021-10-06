732159afc88600b160dbee874f470ff764ce42b4
Victor Ivan <viictor.ivan@gmail.com>
2021-10-06 19:21:34 -0300

    fix: putting relative path on import transaction_bp

--- api/src/blueprints/__init__.py
+++ api/src/blueprints/__init__.py
@@ -2,5 +2,5 @@ from flask import Flask
 
 def init_app(app: Flask):
 
-    from bp_transaction import transaction_bp
+    from .bp_transaction import transaction_bp
     app.register_blueprint(transaction_bp)
