7474b91444cff17bb666ffc590161cd81e953153
Victor Ivan <viictor.ivan@gmail.com>
2021-10-06 19:17:00 -0300

    fix: solving problems to upload files in pytest

--- api/src/tests/test_transaction_controller.py
+++ api/src/tests/test_transaction_controller.py
@@ -1,9 +1,11 @@
 from src.controllers.transaction_controller import TransactionController
+from os import path
 
-posts = {
-    'wrong_file': ('incorrect', open('./files/incorrect', 'rb')),
-    'correct_file': open('./files/CNAB.txt', 'r')
-}
+# absolute path of this file
+here = path.dirname(__file__)
+
+
+correct = open(path.join(here, 'files/CNAB.txt'), 'r')
 
 def test_when_transaction_post_is_empty(client):
     
@@ -14,17 +16,18 @@ def test_when_transaction_post_is_empty(client):
         data={}
     )
     assert resp.status_code == 400
-    assert resp.json() == {'msg': 'You need to upload a file'}
-    
+    assert resp.json == {'msg': 'You need to upload a file'}
+
 
 def test_when_the_type_of_file_is_not_txt(client):
 
+    wrong = open(path.join(here, 'files/incorrect'), 'rb')
     """ If the request was did with a file that is not a .txt,
         then return an error
     """
     resp = client.post('/transaction', 
         headers={'Content-Type': 'multipart/form-data'},
-        data=posts['correct_file']
+        data={'file': wrong }
     )
     assert resp.status_code == 400
-    assert resp.json() == {'msg': 'Your file needs to be a .txt'}
+    assert resp.json == {'msg': 'Your file needs to be a .txt'}
