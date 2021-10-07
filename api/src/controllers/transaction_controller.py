b3bcd9b4c383121f4d978a30fe92d24cd2c69771
Victor Ivan <viictor.ivan@gmail.com>
2021-10-06 21:18:24 -0300

    refactor: creating a function for validate the file uploaded

--- api/src/controllers/transaction_controller.py
+++ api/src/controllers/transaction_controller.py
@@ -1,14 +1,26 @@
 from flask import request, jsonify, current_app
-from werkzeug.utils import validate_arguments
+import re
 
 class TransactionController:
+
+    @staticmethod
+    def check_file(file):
+        reg = "(\d{34})(\*\*\*\*)(\d{10})(.{32})"
+
+        for f, v in file.items():
+            if v.content_type != 'text/plain':
+                raise InvalidFileType()
+                return {'msg': 'Your file needs to be a .txt'}, 400
 
     @staticmethod
     def post(file = None):
         
         if file == None:
             file = request.files
-        for f, v in file.items():
-            if v.content_type != 'text/plain':
-                return {'msg': 'Your file needs to be a .txt'}, 400
+        try:
+            TransactionController.check_file(file)
+        except InvalidFileType as e:
+            return e.msg, e.status
+        
+    
         return {'msg': 'You need to upload a file'}, 400
