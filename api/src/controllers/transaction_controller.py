34d2a0bff753d407f514a80b294f4a4f921b1101
Victor Ivan <viictor.ivan@gmail.com>
2021-10-06 22:22:05 -0300

    chore: adding check for a valid CNAB file

--- api/src/controllers/transaction_controller.py
+++ api/src/controllers/transaction_controller.py
@@ -1,17 +1,33 @@
 from flask import request, jsonify, current_app
-from src.models.errors import InvalidFileType
+from src.models.errors import InvalidFileType, InvalidCNABFile
 import re
 
 class TransactionController:
 
     @staticmethod
-    def check_file(file):
+    def check_line(row):
         reg = "(\d{34})(\*\*\*\*)(\d{10})(.{32})"
+        if re.fullmatch(reg, row) == None:
+            raise InvalidCNABFile()
+        
 
+    @staticmethod
+    def check_file(file):
+    
         for f, v in file.items():
+            uploaded_file = v
             if v.content_type != 'text/plain':
                 raise InvalidFileType()
 
+        file = uploaded_file.read()
+        line = ''
+        for letter in file:
+            if chr(letter) != '\n':
+                line += chr(letter)
+            else:
+                TransactionController.check_line(line)
+                line = ''
+    
     @staticmethod
     def post():
         
@@ -20,8 +36,7 @@ class TransactionController:
             return {'msg': 'You need to upload a file'}, 400
         try:
             TransactionController.check_file(file)
-        except InvalidFileType as e:
+        except (InvalidFileType, InvalidCNABFile) as e:
             return e.msg, e.status
         
-    
         return 'ok'
